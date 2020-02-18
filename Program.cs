e74ccb29d3aedde66814a8ab863950b803ed18d3
DanielGarQ <60907486+DanielGarQ@users.noreply.github.com>
2020-02-17 21:19:13 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CASIO SUPREME");
+            int valor1;
+            int valor2;
+            Console.WriteLine("imgrese un número");
+            valor1 = Console.ReadLine();
+            Console.WriteLine("Ingrese el otro numero dulzura");
+            valor2 = Console.ReadLine();
+            String operador //*,/,-,+
+                switch (operador)
+                case "*":
+                Console.WriteLine (valor1*valor2);break;
+                case"/"
+
+          
+
+        }
+    }
+}
